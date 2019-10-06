2485171246514cb26a0268b52bb65b7ec99530f8
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2019-10-07 00:02:07 +0300

    feat : add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,36 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.arr = [];
+        this.guees = null;
+    }
+    
 
     setRange(min, max) {
-
+        
+        for (var i = min; i < max; i++ ){
+            this.arr.push(i);
+        }
+        // console.log('m - m ', min, max);
+        
     }
 
     guess() {
-
+        this.guees = Math.ceil(this.arr.length / 2);
+        // console.log('guees -- ', this.guees, this.arr[this.guees]);
+        
+        return this.arr[this.guees];
     }
 
     lower() {
-
+        this.arr.splice(this.guees)
+        // console.log('lower');
+        
     }
 
     greater() {
-
+        this.arr.splice(0, this.guees)
+        // console.log('greater');
+        
     }
 }
 
